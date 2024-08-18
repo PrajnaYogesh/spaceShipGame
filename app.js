ae607a600df4f6c8e974250859c18af9b01af8ea
Prajna <prajna.yogish4@gmail.com>
2024-08-18 17:58:43 -0400

    all functionalities work

--- app.js
+++ app.js
@@ -5,14 +5,24 @@ class Player{
         this.firePower=firePower;
         this.accuracy=accuracy;
     }
+
+    reset() {
+        this.name = null;
+        this.hull = null;
+        this.firePower = null;
+        this.accuracy = null;
+    }
     }
 
-  
+    let captain;
+    let alienShip=[]
 
-    let captain = new Player("captain",20,5,0.7);
-    console.log(captain);
-    let alienShip=[];
-
+  function createPlayerAndAliens(){
+   captain = new Player("captain",20,5,0.7);
+    console.log("captain created :",captain);
+    
+    // create aliens
+    // let alienShip=[];
     for(let i=0;i<6;i++){
         let alienName = "alien"+i;
        let alienHull = Math.floor(Math.random() * (6 - 3) + 3)
@@ -22,84 +32,150 @@ class Player{
           alienShip.push(alienShip[i]);
            }
     
+           alienNo=1;
+           alienNumber.innerHTML=` : ${alienNo}`;
+    console.log("alien array created with aliens:");
     console.log(alienShip);
     console.log("******************");
-    
+  }
 
+//   createPlayerAndAliens();
+    
+    
+// target the elements
 let start = document.querySelector('.start') 
 let retreat =  document.querySelector('.retreat')   
 let fireBtn = document.getElementById('fire');
-let currentAlien = alienShip.shift();
-let gameStart;
+ let currentAlien;
 
-console.log(currentAlien);
-
-let alienNo=1;
 let alienNumber = document.querySelector('.alienNo')
 
+// capture captains span - hull,firepower,accuracy
+let captainHullSpan = document.getElementById('captain-hull-span')
+let captainFPSpan = document.getElementById('captain-firepower-span')
+let captainAccuracySpan = document.getElementById('captain-accuracy-span')
 
-function calculateAccuracy(){
-    let random = Math.random();
-    console.log(random);
-    
-    if (random > currentAlien.accuracy ){
-        console.log('you hit the target,reduce opposition\'s hull')
-        return true;
-    }else{
-        console.log('you missed the alien')
-        return false;
-    }
-    
-    }
+// capture aliens hull,firepower,accuracy
+let alienHullSpan = document.getElementById('alien-hull-span')
+let alienFPSpan = document.getElementById('alien-firepower-span')
+let alienAccuracySpan = document.getElementById('alien-accuracy-span')
+
+
+
+let gameStart;
+let alienNo;
 
 
 
 function resetGame(){
     console.log("game reset");
-  let retreatAns= prompt('do you want to retreat?');
+  let retreatAns= confirm('start again?');
  if(retreatAns){
-    console.log("reset to start game");
-   } 
+    gameStart=false;
+        console.log("Game has been reset with empty object");
+    captain.reset();
+    alienShip.length=0;;
+    alienNumber.textContent=0;
+   } else{
+    console.log("should go back to prev state");
+    
    }
+   }
+
+
+// calculate the accuracy with which the capatain hits alien and see if alien was hit
+function calculateAliensAccuracy(){
+    console.log('inside calc accuracy -> alien hits captain');
+    let random = Math.random();
+    console.log('captains accuracy:',random);
+    console.log("current aliens accuracy is: ",currentAlien.accuracy);
+         if (  random < currentAlien.accuracy ){
+    console.log('Alien hit the captain,reduce captains hull points')
+    return true;
+    }else{
+    console.log('alien missed the captain')
+    return false;
+    }
+    
+    }
+
+    function isCaptainDefeated(){
+        if(captain.hull<=0){
+            console.log("captain defeated,alien won");
+            // display Alien won msg here using html
+            alert('Alien won!')
+                         resetGame();
+                 }
+                 else{
+                    console.log("continue game,now captain will fire");
+                    
+                 }
+         
+    }
+
 
    function alienFires(){
     console.log('alien is attacking captain');
-    let ans = calculateAccuracy();
+    let ans = calculateAliensAccuracy();
     if(ans){
-console.log('You missed the traget');
-
+       captain.hull-=currentAlien.firePower;
+       captainHullSpan.textContent=` : ${captain.hull}`;
+       console.log('captain hull points reduced: ',captain);
+    isCaptainDefeated();   
     }else{
-        captain.hull-=currentAlien.firePower;
-        console.log(captain);
-        
+        console.log('You missed the traget,captain value doesnt change,captain can hit firebutton'); 
+      
     }
     
    }
 
 function isDefeated(){
     if(currentAlien.hull<=0){
+        console.log("currentAlien's hull is less than 0,destroy this alien");
+        // put an innerhtml that says alien is destroyed
         currentAlien=alienShip.shift();
+        console.log('New alien popped : ',currentAlien)
         alienNo+=1
-alienNumber.innerHTML=alienNo;
-        console.log(currentAlien);
-        
-        if(!currentAlien){
-            console.log('Captain Won');
-                  }
+         console.log('new alien number : ',alienNo);
+        if(alienNo<=6){
+            alienNumber.innerHTML=` : ${alienNo}`;
+            alienHullSpan.textContent= ` : ${currentAlien.hull}`;
+            alienFPSpan.textContent= `: ${currentAlien.firePower}`;
+            alienAccuracySpan.textContent= `: ${currentAlien.accuracy}`;
+            
         }else{
+            console.log('All aliens defeated,Captain Won');
+                     resetGame();
+        }    
+        }
+        else{
         alienFires();
     }
 }
 
-
+// calculate the accuracy with which the capatain hits alien and see if alien was hit
+function calculateAccuracy(){
+    console.log('inside calc accuracy -> catain hits alien');
+    let random = Math.random();
+    console.log('captains accuracy:',random);
+     if (random > currentAlien.accuracy ){
+    console.log('captain hit the target,reduce aliens hull points')
+    return true;
+    }else{
+    console.log('captain missed the alien')
+    return false;
+    }
+    
+    }
 
 
 function captainFires(){
-    
-    console.log('inside captain function')
-      let answer =  calculateAccuracy();
+  console.log('inside captain function,the captain fires the alien')
+  let answer =  calculateAccuracy();
 if(answer){
     currentAlien.hull-=captain.firePower; 
+    console.log("current Aliens hull reduced",currentAlien.hull);
+    alienHullSpan.textContent=` : ${currentAlien.hull}`;
     isDefeated();
 }else{
     alienFires();
@@ -109,13 +185,28 @@ if(answer){
 
 
 
-
+// do this on clicking fire
 fireBtn.addEventListener('click',captainFires)
 
-start.addEventListener('click', ()=>{
-    console.log('start clicked');
-        gameStart=true;
 
+
+
+// start the game
+start.addEventListener('click', ()=>{
+    console.log('start clicked,game set to true');
+    gameStart=true; 
+    createPlayerAndAliens();
+   currentAlien = alienShip.shift();
+   console.log('first alien popped:',currentAlien);
+   
+//    set intial values:
+captainHullSpan.textContent=` : ${captain.hull}`;
+captainFPSpan.textContent= ` : ${captain.firePower}`;
+captainAccuracySpan.textContent= ` : ${captain.accuracy}`;
+
+alienHullSpan.textContent= ` : ${currentAlien.hull}`;
+alienFPSpan.textContent= `: ${currentAlien.firePower}`;
+alienAccuracySpan.textContent= `: ${currentAlien.accuracy}`;
     
 })
 
